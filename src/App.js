8fe9a09c93ede424fa5c9230bc7524cea8fd6b1d
let <let@lafayette.edu>
2020-03-07 18:01:08 -0500

    First commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
 import React from 'react';
-import logo from './logo.svg';
+import downButton from './assets/down-button.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.state = {varName: 'Thing'};
+  }
+
+  handleChange(event) {
+    this.setState({varName: event.target.value});
+    
 }
+
+  render() {
+    this.varName="Hi"
+    return (
+      <div className="App">
+        <div className="AppContent">
+        <form className="form-horizontal">
+                <p>Type in your variable/class</p>
+                <input type="text" value={this.state.value} onChange={this.handleChange} />
+            </form>
+            
+            
+          <div className="Title"><h2>Type <div className="VarName">{this.state.varName}</div> was not found</h2></div>
+          <p>
+          Translation: You are trying to declare a variable of type <div className="VarName">{this.state.varName}</div> but I don’t know what a <div className="VarName">{this.state.varName}</div> is.
+          </p>
+          <div className="ErrorTile">
+            <div className="ErrorMessage">
+              <h4>1. You may have mistyped class name <div className="VarName">{this.state.varName}</div> </h4>
+              <p>Hint: Is <div className="VarName">{this.state.varName}</div> the exact class name that you want?</p>
+            </div>
+
+            <div className="ButtonHolder"><img src={downButton} alt="Drop-down-button" width="30" height="30"></img></div>
+          </div>
+          
+        </div>
+      </div>
+    );
+  }
+
+}
+
+
 
 export default App;
