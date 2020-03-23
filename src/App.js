174a81a248078edab953126841701610653c89c9
let <let@lafayette.edu>
2020-03-23 15:44:26 -0400

    Add react router to navigate between different pages

--- src/App.js
+++ src/App.js
@@ -1,17 +1,46 @@
 import React from "react";
 import "./App.css";
 import TypeNotFound from "./TypeNotFound.jsx";
+import HomePage from "./HomePage.jsx"
+import HomeButton from "./assets/home.svg";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
+export default function App() {
+  return (
+    <Router>
+      <div>
+        <div className="Home">
+          <Link to="/">
+            <img
+              src={HomeButton}
+              alt="home-button"
+              width="30"
+              height="30"
+            ></img>
+          </Link>
+        </div>
+        
 
-class App extends React.Component {
-
-  render() {
-    return (
-        <div>
-          <TypeNotFound/>
-        </div> 
-    );
-  }
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/typenotfound">
+            <TypeNotFound />
+          </Route>
+          <Route path="/">
+            <HomePage />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
 }
 
-export default App;
+
+
+
---
+++ src/HomePage.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./App.css";
+import {
+  Link
+} from "react-router-dom";
+
+class HomePage extends React.Component {
+
+  render() {
+    return (
+        <div className="App">
+            <div className="AppContent">
+                <h2>List of Errors</h2>
+                <div className="Indent-1"><Link to="/typenotfound">Type Not Found</Link> </div>
+            </div>
+        </div>
+    );
+  }
+}
+
+
+export default HomePage;
