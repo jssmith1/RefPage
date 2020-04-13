e91fa9f7eaca9f122ae7c9150e3ac8a054f7646b
let <let@lafayette.edu>
2020-04-13 16:19:14 -0400

    Updates

---
+++ precache-manifest.e8d02cd7b50cd6fa5f70399de28deecf.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "060e8267dad13222a509b6f68d0cdeea",
+    "url": "/RefPage/index.html"
+  },
+  {
+    "revision": "e0bf9369b6d36a434d22",
+    "url": "/RefPage/static/css/main.80f10025.chunk.css"
+  },
+  {
+    "revision": "33c232109cc71d06d581",
+    "url": "/RefPage/static/js/2.f203f172.chunk.js"
+  },
+  {
+    "revision": "b89c29235bff0deb3d901915226b52db",
+    "url": "/RefPage/static/js/2.f203f172.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0bf9369b6d36a434d22",
+    "url": "/RefPage/static/js/main.2d7f94ad.chunk.js"
+  },
+  {
+    "revision": "d6775ca5ef8249c53320",
+    "url": "/RefPage/static/js/runtime-main.d35e7625.js"
+  },
+  {
+    "revision": "1a4fd13953b4b72509baa0154ff2a132",
+    "url": "/RefPage/static/media/home.1a4fd139.svg"
+  },
+  {
+    "revision": "d6f7642b18caf374f59e0a51a0b13d31",
+    "url": "/RefPage/static/media/minus.d6f7642b.svg"
+  },
+  {
+    "revision": "94676a1348b66935905228b76a042b7f",
+    "url": "/RefPage/static/media/plus.94676a13.svg"
+  }
+]);
