3c096a09b85f6fec0d4f8b68624f02e197113610
soir20 <71418127+soir20@users.noreply.github.com>
2021-06-30 17:47:43 -0400

    Refactor variable declarators page

--- src/components/errors/SyntaxErrorVariableDeclarators.jsx
+++ src/components/errors/SyntaxErrorVariableDeclarators.jsx
@@ -1,346 +1,58 @@
 import React from "react";
-import MinusButton from "../../assets/minus.svg";
-import PlusButton from "../../assets/plus.svg";
 import "../../App.css";
-import BlueCheckbox from '../resources/blueCheckbox'
+import CompilerError from "../CompilerError";
+import Problem from "../Problem";
+import Suggestion from "../Suggestion";
+import BadExample from "../BadExample";
+import GoodExample from "../GoodExample";
 
 class SyntaxErrorVariableDeclarators extends React.Component {
-  constructor(props) {
-    super(props);
-    this.openStrategyTile = this.openStrategyTile.bind(this);
-    this.openCodeExample = this.openCodeExample.bind(this);
-    this.changeChecked = this.changeChecked.bind(this);
-    this.state = {
-      openStrategy1: false,
-      openStrategy2: false,
-      openCode11: false,
-      openCode21: false,
-      checked11: false,
-      checked21: false,
-    };
-  }
-
-  openStrategyTile(i) {
-    switch (i) {
-      case 1:
-        this.setState({ openStrategy1: !this.state.openStrategy1 });
-        this.setState({ openCode11: false });
-        break;
-      case 2:
-        this.setState({ openStrategy2: !this.state.openStrategy2 });
-        this.setState({ openCode21: false });
-        break;
-      default:
-        break;
-    }
-  }
-
-  openCodeExample(i) {
-    switch (i) {
-      case 11:
-        this.setState({ openCode11: !this.state.openCode11 });
-        break;
-      case 21:
-        this.setState({ openCode21: !this.state.openCode21 });
-        break;
-      default:
-        break;
-    }
-  }
-
-  openStrategyTileBoxOnly(i) {
-    switch (i) {
-      case 1:
-        if(!this.state.openStrategy1){
-          this.setState({ openStrategy1: true });
-          this.setState({ openCode11: false });
-        }
-        break;
-      case 2:
-        if(!this.state.openStrategy2){
-          this.setState({ openStrategy2: true });
-          this.setState({ openCode21: false });
-        }
-        break;
-      default:
-        break;
-    }
-  }
-
-  openCodeExampleBoxOnly(i) {
-    switch (i) {
-      case 11:
-        if(!this.state.openCode11){
-          this.setState({ openCode11: !this.state.openCode11 });
-        }
-        break;
-      case 21:
-        if(!this.state.openCode21){
-          this.setState({ openCode21: !this.state.openCode21 });
-        }
-        break;
-      default:
-        break;
-    }
-  }
-
-  changeChecked(i) {
-    switch (i) {
-      case 11:
-        if(this.state.openCode11){
-          if(!this.state.checked11){
-          this.setState({ checked11: !this.state.checked11 });
-          this.setState({ openCode11: false });
-          } else {
-          this.setState({ checked11: !this.state.checked11 });
-          this.setState({ openCode11: true });
-          }
-        }
-        if(!this.state.openCode11){
-          this.setState({ checked11: !this.state.checked11 });
-          this.setState({ openCode11: false });
-        }
-        break;
-      case 21:
-        if(this.state.openCode21){
-          if(!this.state.checked21){
-          this.setState({ checked21: !this.state.checked21 });
-          this.setState({ openCode21: false });
-          } else {
-          this.setState({ checked21: !this.state.checked21 });
-          this.setState({ openCode21: true });
-          }
-        }
-        if(!this.state.openCode21){
-          this.setState({ checked21: !this.state.checked21 });
-          this.setState({ openCode21: false });
-        }
-        break;
-      default:
-        break;
-    }
-  }
 
   render() {
-    return (
-      <div className="App">
-        <div className="AppContent">
-          <div className="Title">
-            <h2>
-                Syntax error, insert "VariableDeclarators" to complete LocalVariableDeclaration
-            </h2>
-          </div>
+    const trimmedType = this.props.typeName.substring(this.props.typeName.lastIndexOf('.') + 1);
 
-          <h4>
-            <i>
-              Translation: A syntax error occured while declaring a variable.
-            </i>
-          </h4>
-
-          <div className="Tile">
-            <div className="ErrorTile" onClick={() => this.openStrategyTileBoxOnly(1)}>
-              <div className="ErrorMessage">
-                <h4>
-                  1: It looks like Processing is reading the <div className="InputValue">{this.props.methodOneName} </div> method {" "}
-                  as a variable. You may have forgotten to add parentheses when writing {" "}
-                  <div className="InputValue">{this.props.methodOneName} </div>
-                </h4>
-              </div>
-
-              {!this.state.openStrategy1 && (
-                <div className="ButtonHolder">
-                  <img
-                    onClick={() => this.openStrategyTile(1)}
-                    src={PlusButton}
-                    alt="down-button"
-                    width="20"
-                    height="20"
-                  ></img>
-                </div>
-              )}
-
-              {this.state.openStrategy1 && (
-                <div className="ButtonHolder">
-                  <img
-                    onClick={() => this.openStrategyTile(1)}
-                    src={MinusButton}
-                    alt="up-button"
-                    width="20"
-                    height="20"
-                  ></img>
-                </div>
-              )}
-
-            </div>
-            {this.state.openStrategy1 && (
-              <div className="StrategyContainer">
-                <i>Tick the box once you have tried the suggestion</i>
-                <div
-                  className="StrategyTile"
-                  onClick={() => this.openCodeExampleBoxOnly(11)}
-                >
-                  <div className="StrategyInstruction">
-                    <div className="StrategyMessage">
-                      <BlueCheckbox
-                        value="box1"
-                        checked={this.state.checked11}
-                        onChange={() => this.changeChecked(11)}
-                      />
-                      <div className="Suggestion">
-                         Suggestion 1: Add "()" to the end of {" "}
-                         <div className="InputValue">{this.props.methodOneName} </div>
-                      </div>
-                    </div>
-                    {!this.state.openCode11 && (
-                      <div className="ButtonHolder">
-                        <img
-                          onClick={() => this.openCodeExample(11)}
-                          src={PlusButton}
-                          alt="down-button"
-                          width="20"
-                          height="20"
-                        ></img>
-                      </div>
-                    )}
-                    {this.state.openCode11 && (
-                      <div className="ButtonHolder">
-                        <img
-                          onClick={() => this.openCodeExample(11)}
-                          src={MinusButton}
-                          alt="up-button"
-                          width="20"
-                          height="20"
-                        ></img>
-                      </div>
-                    )}
-                  </div>
-                  {this.state.openCode11 && (
-                    <div className="CodeExample">
-                      <div className="CodeContainer">
-                        <div className="RedCode">
-                          <div className="Indent-0">
-                            {this.props.methodOneName};
-                          </div>
-                        </div>
-                      </div>
-                      <div className="CodeContainer">
-                        <div className="GreenCode">
-                        <div className="Indent-0">
-                        {this.props.methodOneName}();
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  )}
-                </div>
-              </div>
-            )}
-          </div>
-
-          <div className="Tile">
-            <div className="ErrorTile" onClick={() => this.openStrategyTileBoxOnly(2)}>
-              <div className="ErrorMessage">
-                <h4>
-                  2: You may have forgotten to fully write the body of the method {" "}
-                  <div className="InputValue">{this.props.methodTwoName}() </div>
-                </h4>
-              </div>
-
-              {!this.state.openStrategy2 && (
-                <div className="ButtonHolder">
-                  <img
-                    onClick={() => this.openStrategyTile(2)}
-                    src={PlusButton}
-                    alt="down-button"
-                    width="20"
-                    height="20"
-                  ></img>
-                </div>
-              )}
-
-              {this.state.openStrategy2 && (
-                <div className="ButtonHolder">
-                  <img
-                    onClick={() => this.openStrategyTile(2)}
-                    src={MinusButton}
-                    alt="up-button"
-                    width="20"
-                    height="20"
-                  ></img>
-                </div>
-              )}
-
-            </div>
-            {this.state.openStrategy2 && (
-              <div className="StrategyContainer">
-                <i>Tick the box once you have tried the suggestion</i>
-                <div
-                  className="StrategyTile"
-                  onClick={() => this.openCodeExampleBoxOnly(21)}
-                >
-                  <div className="StrategyInstruction">
-                    <div className="StrategyMessage">
-                      <BlueCheckbox
-                        value="box1"
-                        checked={this.state.checked21}
-                        onChange={() => this.changeChecked(21)}
-                      />
-                      <div className="Suggestion">
-                         Suggestion 1: Specify what {" "}
-                         <div className="InputValue">{this.props.methodTwoName}() </div> should do
-                      </div>
-                    </div>
-                    {!this.state.openCode21 && (
-                      <div className="ButtonHolder">
-                        <img
-                          onClick={() => this.openCodeExample(21)}
-                          src={PlusButton}
-                          alt="down-button"
-                          width="20"
-                          height="20"
-                        ></img>
-                      </div>
-                    )}
-                    {this.state.openCode21 && (
-                      <div className="ButtonHolder">
-                        <img
-                          onClick={() => this.openCodeExample(21)}
-                          src={MinusButton}
-                          alt="up-button"
-                          width="20"
-                          height="20"
-                        ></img>
-                      </div>
-                    )}
-                  </div>
-                  {this.state.openCode21 && (
-                    <div className="CodeExample">
-                      <div className="CodeContainer">
-                        <div className="RedCode">
-                          <div className="Indent-0"> {this.props.typeName} s = 5; </div>
-                          <div className="Indent-0"> void {this.props.methodTwoName}({this.props.typeName} x) {"{"}</div>
-                          <div className="Indent-1"> s; </div>
-                          <div className="Indent-0"> {"}"}</div>
-                        </div>
-                      </div>
-                      <div className="CodeContainer">
-                        <div className="GreenCode">
-                        <div className="Indent-0"> {this.props.typeName} s = 5; </div>
-                          <div className="Indent-0"> void {this.props.methodTwoName}({this.props.typeName} x) {"{"}</div>
-                          <div className="Indent-1"> s = s*x; </div>
-                          <div className="Indent-0"> {"}"}</div>
-                        </div>
-                      </div>
-                    </div>
-                  )}
-                </div>
-              </div>
-            )}
-          </div>
-        </div>
-      </div>
-    );
+    return <CompilerError
+      title={"Syntax error, insert \"VariableDeclarators\" to complete LocalVariableDeclaration"}
+      translation="A syntax error occured while declaring a variable."
+    >
+      <Problem
+        title={<>It looks like Processing is reading the <div className="InputValue">{this.props.methodOneName} </div> method {" "}
+          as a variable. You may have forgotten to add parentheses when writing {" "}
+          <div className="InputValue">{this.props.methodOneName}</div>.</>}
+      >
+        <Suggestion title={<>Add "()" to the end of {" "}
+          <div className="InputValue">{this.props.methodOneName}</div>.</>}>
+          <BadExample>
+            <div className="Indent-0">{this.props.methodOneName};</div>
+          </BadExample>
+          <GoodExample>
+            <div className="Indent-0">{this.props.methodOneName}();</div>
+          </GoodExample>
+        </Suggestion>
+      </Problem>
+      <Problem
+        title={<>You may have forgotten to fully write the body of the method {" "}
+          <div className="InputValue">{this.props.methodTwoName}()</div>.</>}
+      >
+        <Suggestion title={<>Specify what {" "}
+          <div className="InputValue">{this.props.methodTwoName}() </div> should do.</>}>
+          <BadExample>
+            <div className="Indent-0"> {trimmedType} s = 5; </div>
+            <div className="Indent-0"> void {this.props.methodTwoName}({trimmedType} x) {"{"}</div>
+            <div className="Indent-1"> s; </div>
+            <div className="Indent-0"> {"}"}</div>
+          </BadExample>
+          <GoodExample>
+            <div className="Indent-0"> {this.props.typeName} s = 5; </div>
+            <div className="Indent-0"> void {this.props.methodTwoName}({trimmedType} x) {"{"}</div>
+            <div className="Indent-1"> s = s * x; </div>
+            <div className="Indent-0"> {"}"}</div>
+          </GoodExample>
+        </Suggestion>
+      </Problem>
+    </CompilerError>;
   }
+
 }
 
 export default SyntaxErrorVariableDeclarators;
