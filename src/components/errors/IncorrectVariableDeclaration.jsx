94f51ead01d7c189444d953f011ab3e2e428d4ec
soir20 <71418127+soir20@users.noreply.github.com>
2021-07-07 18:56:23 -0400

    Remove nested conditionals

--- src/components/errors/IncorrectVariableDeclaration.jsx
+++ src/components/errors/IncorrectVariableDeclaration.jsx
@@ -9,8 +9,31 @@ import GoodExample from "../GoodExample";
 class IncorrectVariableDeclaration extends React.Component {
 
   render() {
-    const isPrimitive = this.props.typeName === 'String' || this.props.typeName === 'char' || this.props.typeName === 'boolean' || this.props.typeName === 'short' || this.props.typeName === 'byte' ||
-      this.props.typeName === 'long' || this.props.typeName === 'int' || this.props.typeName === 'float' || this.props.typeName === 'double';
+    const knownTypes = ["String", "char", "boolean", "short", "byte", "long", "int", "float", "double"];
+    const isKnownType = knownTypes.includes(this.props.typeName);
+
+    const demoArrays = {
+      String: "{\"a\", \"b\", \"c\", \"d\", \"e\"}",
+      char: "{'a', 'b', 'c', 'd', 'e'}",
+      boolean: "{true, false, true, false, true}",
+      byte: "{1, 2, 3, 4, 5}",
+      short: "{1, 2, 3, 4, 5}",
+      int: "{1, 2, 3, 4, 5}",
+      long: "{1, 2, 3, 4, 5}",
+      float: "{1.1, 2.2, 3.3, 4.4, 5.5}",
+      double: "{1.1, 2.2, 3.3, 4.4, 5.5}"
+    };
+
+    let unknownTypeArray = "{";
+    for (let i = 0; i < 5; i++) {
+      unknownTypeArray += `new ${this.props.typeName}(), `;
+    }
+    unknownTypeArray = unknownTypeArray.substring(0, unknownTypeArray.length - 2) + "}";
+
+    const demoArray = isKnownType ? demoArrays[this.props.typeName] : unknownTypeArray;
+
+    // eslint-disable-next-line
+    const classPlaceholder = isKnownType ? "" : <div className="Indent-0">class {this.props.typeName} {"{"}/* your code */{"}"} </div>;
 
     return <CompilerError
       title={<>Expecting DOT, found <div className="InputValue">'{this.props.foundName}'</div></>}
@@ -22,170 +45,68 @@ class IncorrectVariableDeclaration extends React.Component {
       >
         <Suggestion title="Add the word 'new' beside the type variable you are declaring.">
           <BadExample>
-            {isPrimitive ?
-              <div className="Indent-0"> {this.props.typeName}[] {this.props.foundName} = {this.props.typeName}[5]; </div>
-              : <div className="Indent-0"> int[] {this.props.foundName} = int[5]; </div>
-            }
+            {classPlaceholder}
+            <div className="Indent-0"> {this.props.typeName}[] {this.props.foundName} = {this.props.typeName}[5]; </div>
           </BadExample>
 
           <GoodExample>
-            {isPrimitive ?
-              <div className="Indent-0"> {this.props.typeName}[] {this.props.foundName} = new {this.props.typeName}[5]; </div>
-              : <div className="Indent-0"> int[] {this.props.foundName} = new int[5]; </div>
-            }
+            {classPlaceholder}
+            <div className="Indent-0"> {this.props.typeName}[] {this.props.foundName} = new {this.props.typeName}[5]; </div>
           </GoodExample>
         </Suggestion>
         <Suggestion title={"Add a semicolon \";\" at the end of declaration."}>
           <BadExample>
-            {isPrimitive ?
-              <div className="Indent-0"> {this.props.typeName}[] {this.props.foundName} new {this.props.typeName}[5] </div>
-              : <div className="Indent-0"> int[] {this.props.foundName} new int[5] </div>
-            }
+            {classPlaceholder}
+            <div className="Indent-0"> {this.props.typeName}[] {this.props.foundName} = new {this.props.typeName}[5] </div>
           </BadExample>
 
           <BadExample>
-            <div className="Indent-0">
-              {this.props.typeName === 'String'            //Box 1 Ex 2 for String
-                ?
-                <>{this.props.typeName}[] {this.props.foundName} = {"{"}"a", "b", "c", "d", "e"{"}"}</>
-
-                : this.props.typeName === 'char'            //Box 1 Ex 2 for char
-                  ?
-                  <>{this.props.typeName}[] {this.props.foundName} = {"{'a', 'b, 'c', 'd', 'e'}"}</>
-
-                  : this.props.typeName === 'boolean'            //Box 1 Ex 2 for boolean
-                    ?
-                    <>{this.props.typeName}[] {this.props.foundName} = {"{true, false, true, false, true}"}</>
-
-                    : this.props.typeName === 'float' || this.props.typeName === 'double'            //Box 1 Ex 2 for float/double
-                      ?
-                      <>{this.props.typeName}[] {this.props.foundName} = {"{1.1, 2.2, 3.3, 4.4, 5.5}"}</>
-
-                      : this.props.typeName === 'int' || this.props.typeName === 'short' || this.props.typeName === 'byte'             //Box 1 Ex 2 for int/short/byte/long
-                        ?
-                        <>{this.props.typeName}[] {this.props.foundName} = {"{1,2,3,4,5}"}</>
-                        :
-                        <>int[] {this.props.foundName} = {"{1,2,3,4,5}"}</>
-              }
-            </div>
+            {classPlaceholder}
+            <div className="Indent-0"> {this.props.typeName}[] {this.props.foundName} = {demoArray} </div>
           </BadExample>
 
           <GoodExample>
-            {isPrimitive ?
-              <div className="Indent-0"> {this.props.typeName}[] {this.props.foundName} = new {this.props.typeName}[5]; </div>
-              : <div className="Indent-0"> int[] {this.props.foundName} = {" "}new int[5]; </div>
-            }
+            {classPlaceholder}
+            <div className="Indent-0"> {this.props.typeName}[] {this.props.foundName} = new {this.props.typeName}[5]; </div>
           </GoodExample>
 
           <GoodExample>
-            <div className="Indent-0">
-              {this.props.typeName === 'String'            //Box 1 Ex 2 for String
-                ?
-                <>{this.props.typeName}[] {this.props.foundName} = {"{"}"a", "b", "c", "d", "e"{"}"};</>
-
-                : this.props.typeName === 'char'            //Box 1 Ex 2 for char
-                  ?
-                  <>{this.props.typeName}[] {this.props.foundName} = {"{'a', 'b, 'c', 'd', 'e'}"};</>
-
-                  : this.props.typeName === 'boolean'            //Box 1 Ex 2 for boolean
-                    ?
-                    <>{this.props.typeName}[] {this.props.foundName} = {"{true, false, true, false, true}"};</>
-
-                    : this.props.typeName === 'float' || this.props.typeName === 'double'            //Box 1 Ex 2 for float/double
-                      ?
-                      <>{this.props.typeName}[] {this.props.foundName} = {"{1.1, 2.2, 3.3, 4.4, 5.5}"};</>
-
-                      : this.props.typeName === 'int' || this.props.typeName === 'short' || this.props.typeName === 'byte'
-                        ?                                                              //Box 1 Ex 2 for int/short/byte/long
-                        <>{this.props.typeName}[] {this.props.foundName} = {"{1,2,3,4,5}"};</>
-                        :
-                        <>int[] {this.props.foundName} = {"{1,2,3,4,5}"};</>
-              }
-            </div>
+            {classPlaceholder}
+            <div className="Indent-0"> {this.props.typeName}[] {this.props.foundName} = {demoArray}; </div>
           </GoodExample>
         </Suggestion>
         <Suggestion title={"Add an equal sign \"=\" when declaring."}>
           <BadExample>
-            {isPrimitive ?
-              <div className="Indent-0"> {this.props.typeName}[] {this.props.foundName} new {this.props.typeName}[5]; </div>
-              : <div className="Indent-0"> int[] {this.props.foundName} new int[5]; </div>
-            }
+            {classPlaceholder}
+            <div className="Indent-0"> {this.props.typeName}[] {this.props.foundName} new {this.props.typeName}[5]; </div>
           </BadExample>
 
           <BadExample>
+            {classPlaceholder}
             <div className="Indent-0">
-              {this.props.typeName === 'String'            //Box 1 Ex 3 for String
-                ?
-                <>{this.props.typeName}[] {this.props.foundName} {"{"}"a", "b", "c", "d", "e"{"}"};</>
-
-                : this.props.typeName === 'char'            //Box 1 Ex 3 for char
-                  ?
-                  <>{this.props.typeName}[] {this.props.foundName} {"{'a', 'b, 'c', 'd', 'e'}"};</>
-
-                  : this.props.typeName === 'boolean'            //Box 1 Ex 3 for boolean
-                    ?
-                    <>{this.props.typeName}[] {this.props.foundName} {"{true, false, true, false, true}"};</>
-
-                    : this.props.typeName === 'float' || this.props.typeName === 'double'            //Box 1 Ex 3 for float/double
-                      ?
-                      <>{this.props.typeName}[] {this.props.foundName} {"{1.1, 2.2, 3.3, 4.4, 5.5}"};</>
-
-                      : this.props.typeName === 'int' || this.props.typeName === 'short' || this.props.typeName === 'byte'
-                        ?                                                            //Box 1 Ex 3 for int/short/byte/long
-                        <>{this.props.typeName}[] {this.props.foundName} {"{1,2,3,4,5}"};</>
-                        :
-                        <>int[] {this.props.foundName} {"{1,2,3,4,5}"};</>
-              }
+              {this.props.typeName}[] {this.props.foundName} {demoArray};
             </div>
           </BadExample>
 
           <GoodExample>
-            {isPrimitive ?
-              <div className="Indent-0"> {this.props.typeName}[] {this.props.foundName} = new {this.props.typeName}[5]; </div>
-              : <div className="Indent-0"> int[] {this.props.foundName} = new int[5]; </div>
-            }
+            {classPlaceholder}
+            <div className="Indent-0"> {this.props.typeName}[] {this.props.foundName} = new {this.props.typeName}[5]; </div>
           </GoodExample>
 
           <GoodExample>
-            <div className="Indent-0">
-              {this.props.typeName === 'String'            //Box 1 Ex 3 for String
-                ?
-                <>{this.props.typeName}[] {this.props.foundName} = {"{"}"a", "b", "c", "d", "e"{"}"};</>
-
-                : this.props.typeName === 'char'            //Box 1 Ex 3 for char
-                  ?
-                  <>{this.props.typeName}[] {this.props.foundName} = {"{'a', 'b, 'c', 'd', 'e'}"};</>
-
-                  : this.props.typeName === 'boolean'            //Box 1 Ex 3 for boolean
-                    ?
-                    <>{this.props.typeName}[] {this.props.foundName} = {"{true, false, true, false, true}"};</>
-
-                    : this.props.typeName === 'float' || this.props.typeName === 'double'            //Box 1 Ex 3 for float/double
-                      ?
-                      <>{this.props.typeName}[] {this.props.foundName} = {"{1.1, 2.2, 3.3, 4.4, 5.5}"};</>
-
-                      : this.props.typeName === 'int' || this.props.typeName === 'short' || this.props.typeName === 'byte'
-                        ?                                                              //Box 1 Ex 3 for int/short/byte/long
-                        <>{this.props.typeName}[] {this.props.foundName} = {"{1,2,3,4,5}"};</>
-                        :
-                        <>int[] {this.props.foundName} = {"{1,2,3,4,5}"};</>
-              }
-            </div>
+            {classPlaceholder}
+            <div className="Indent-0"> {this.props.typeName}[] {this.props.foundName} = {demoArray}; </div>
           </GoodExample>
         </Suggestion>
         <Suggestion title="Specify the size of the array.">
           <BadExample>
-            {isPrimitive ?
-              <div className="Indent-0"> {this.props.typeName}[] {this.props.foundName} = new {this.props.typeName}; </div>
-              : <div className="Indent-0"> int[] {this.props.foundName} = new int; </div>
-            }
+            {classPlaceholder}
+            <div className="Indent-0"> {this.props.typeName}[] {this.props.foundName} = new {this.props.typeName}; </div>
           </BadExample>
 
           <GoodExample>
-            {isPrimitive ?
-              <div className="Indent-0"> {this.props.typeName}[] {this.props.foundName} = new {this.props.typeName}[5]; </div>
-              : <div className="Indent-0"> int[] {this.props.foundName} = new int[5]; </div>
-            }
+            {classPlaceholder}
+            <div className="Indent-0"> {this.props.typeName}[] {this.props.foundName} = new {this.props.typeName}[5]; </div>
           </GoodExample>
         </Suggestion>
       </Problem>
